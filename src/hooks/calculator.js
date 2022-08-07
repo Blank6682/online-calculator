70759e797c677b45f14112e2906f0c0a60ce5784
XiQuan Wu <2452402628@qq.com>
2022-08-08 02:57:40 +0800

    feat : rule init

--- src/hooks/calculator.js
+++ src/hooks/calculator.js
@@ -1,6 +1,19 @@
 import { Status } from "./status"
+import { rule } from './rule';
+import { logMaker } from '../utils/log';
+import { counter } from '../utils/counter';
+import { dom } from './dom';
+import { math } from './math';
+import { animation } from './animation';
+
+const log = logMaker(true)
 
 class Calcuator{
+  
+  constructor() {
+    this.rule = rule
+  }
+
   //分析每次输入，给出相应的行为，Info为db.json中的数据
   analysisEachTimeInput(curInputInfo) {
     //生成当前输入的所有信息
@@ -14,14 +27,59 @@ class Calcuator{
   }
   
   //检测规则
-  _checkRules() {
-    
+  _checkRules(curInputInfo) {
+    //log
+    this._checkRulesLog(curInputInfo)
+    //假设全部通过的标识位，初始true,即全部通过
+    let passAllRulesFlag = true
+
+    curInputInfo.rule.every(ruleName => {
+      //根据规则名称生成具体的函数名
+      const ruleFnName = this._generateRuleFnName(ruleName)
+
+      if (this._ruleFnExists(ruleFnName)) {
+        passAllRulesFlag &&=this._ruleFnExecute(ruleFnName)
+      } else {
+        passAllRulesFlag = false
+
+        log(`Cannot find this function: ${ruleFnName}`,'warn')
+      }
+      return passAllRulesFlag
+    })
+    return passAllRulesFlag
+  }
+
+  //log
+  _checkRulesLog = (curInputInfo) => {
+    const value = counter.next().value
+    const tag = curInputInfo.tag
+    log(`=== Counter:[${value}] Input:${tag} ===`)
+  }
+  //生成规则函数名
+  _generateRuleFnName = (ruleFnName) => {
+    return `this.rule.${ruleFnName}`
+  }
+  //是否存在这个函数
+  _ruleFnExists = (ruleFnName) => {
+    return typeof eval(ruleFnName) ==='function'
+  }
+  //执行函数
+  _ruleFnExecute = (ruleFnName) => {
+    return eval(ruleFnName)()
   }
 
   //执行行为
   _run() {
-    
+    this._addValue()
+    this._showResult()
+    this._clearAll()
+    this._clearLast()
   }
+
+  _addValue = () =>{}
+  _showResult = () =>{}
+  _clearAll = () =>{}
+  _clearLast = () =>{}
 }
 
 const calcuator = new Calcuator()
